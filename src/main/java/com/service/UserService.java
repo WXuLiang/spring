c169015dd028f54e135d119e7364ebf07aba0c0a
Administrator <1329728631@qq.com>
2018-05-06 20:30:15 +0800

    1

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -13,7 +13,7 @@ public class UserService {
     @Autowired
     private UserBean userBean;
     public void printUser(){
-        userBean.setName("张三");
+        userBean.setName("张三2");
         System.out.println(userBean);
     }
 }
